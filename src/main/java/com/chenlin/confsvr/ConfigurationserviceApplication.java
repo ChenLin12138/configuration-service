90fa3dbb2d4c6ef363cf4afe0042180a91b42f88
ChenLin12138 <scorpion_chenlin@163.com>
2019-08-21 15:24:48 +0800

    配置config服务器

--- src/main/java/com/chenlin/confsvr/ConfigurationserviceApplication.java
+++ src/main/java/com/chenlin/confsvr/ConfigurationserviceApplication.java
@@ -2,8 +2,11 @@ package com.chenlin.confsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+//使得服务称为spring cloud config服务
+@EnableConfigServer
 public class ConfigurationserviceApplication {
 
 	public static void main(String[] args) {
